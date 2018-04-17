3d8acda4357843d924cf2700ddd2a88ce4ed595a
yinyayun <yinyayun1314@gmail.com>
2018-04-17 10:27:31 +0800

    添加注释

--- toolbox-algorithm/src/main/java/org/yinyayun/sort/QuickSort.java
+++ toolbox-algorithm/src/main/java/org/yinyayun/sort/QuickSort.java
@@ -1,16 +1,17 @@
 package org.yinyayun.sort;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * 快速排序的核心思想：<br>
+ * （1）设置基准 （2）从后向前找比基准小位置 （3）从前向后找比基准大的位置 （4）交换位置 <br>
+ * 循环进行，直到两个位置重合，一次迭代后小元素靠前大元素靠后
+ * 
+ * @author yinyayun
+ *
+ * @param <T>
+ */
 public class QuickSort<T> extends Sort<T> {
-	public static void main(String[] args) {
-		Comparator<Integer> c = (c1, c2) -> c1 - c2;
-		QuickSort<Integer> sort = new QuickSort<Integer>(c);
-		Integer[] arrays = new Integer[] { 10, 2, 70, 4, 23, 24, 56, 6 };
-		sort.sort(arrays);
-		System.out.println(Arrays.toString(arrays));
-	}
 
 	public QuickSort(Comparator<T> c) {
 		super(c);
@@ -28,16 +29,19 @@ public class QuickSort<T> extends Sort<T> {
 		int l = left, r = right;
 		T base = array[l];
 		while (l != r) {
-			// 从后向前，小于基准的移动至前面
+			// 从后向前，找到小于基准的位置
 			while (l < r && compare(array[r], base) < 0) {
 				r--;
 			}
+			// 从前向后，找到大于基准的位置
 			while (l < r && compare(array[l], base) > 0) {
 				l++;
 			}
+			// 对于找到的两个位置进行交换
 			if (l < r)
 				swap(array, l, r);
 		}
+		// 分区间进行同样操作
 		quickSort(array, left, l - 1);
 		quickSort(array, r + 1, right);
 	}
--- toolbox-algorithm/src/test/java/org/yinyayun/sort/SortTest.java
+++ toolbox-algorithm/src/test/java/org/yinyayun/sort/SortTest.java
@@ -6,8 +6,18 @@ import java.util.Comparator;
 public class SortTest {
 	public static void main(String[] args) {
 		Comparator<Integer> c = (c1, c2) -> c1 - c2;
-		Sort<Integer> sort = new BubblingSort<Integer>(c);
 		Integer[] arrays = new Integer[] { 10, 2, 70, 4, 23, 24, 56, 6 };
+		quickSort(c, arrays);
+	}
+
+	public static void quickSort(Comparator<Integer> c, Integer[] arrays) {
+		Sort<Integer> sort = new QuickSort<>(c);
+		sort.sort(arrays);
+		System.out.println(Arrays.toString(arrays));
+	}
+
+	public static void bubblingSort(Comparator<Integer> c, Integer[] arrays) {
+		Sort<Integer> sort = new BubblingSort<>(c);
 		sort.sort(arrays);
 		System.out.println(Arrays.toString(arrays));
 	}
